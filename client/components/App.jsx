cc383967fc5c72a1c45af68d83c6402e7cb5738c
hera <hera.lindsay@gmail.com>
2022-11-03 16:40:08 +1300

    setup pages

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Balloon from './Balloon'
+import { Route, Routes } from 'react-router-dom'
 function App() {
+  // const newArray = []
+  // for (let i = 0; i < 99; i++) {
+  //   newArray.push(<Balloon />)
+  // }
+  // return newArray
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <Routes>
+        <Route path="/" element={<Balloon />}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ client/components/Balloon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Balloon = () => {
+  return (
+    <div>
+      <h1>This is a balloon</h1>
+    </div>
+  )
+}
+
+export default Balloon
