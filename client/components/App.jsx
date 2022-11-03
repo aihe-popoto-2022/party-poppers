6f57aca5dd1085076d7e75dceb6ff3cae71133f2
Abdifatah Abdilahi <abdifatahabdilahi@OSMANs-MacBook-Pro.local>
2022-11-03 21:22:07 +1300

    popping circles

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,6 @@ import React from 'react'
 import Balloon from './Balloon'
 import { Route, Routes } from 'react-router-dom'
 function App() {
-  // const newArray = []
-  // for (let i = 0; i < 99; i++) {
-  //   newArray.push(<Balloon />)
-  // }
-  // return newArray
   return (
     <div>
       <Routes>
--- client/components/Balloon.jsx
+++ client/components/Balloon.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Balloon = () => {
-  return <h1>im a balloon</h1>
+function Balloon() {
+  const colors = ['red', 'green', 'blue']
+  const generateColor = () => {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+  }
+  const balloonArr = Array(25)
+    .fill()
+    .map((_, i) => ({ id: i, popped: false, color: generateColor() }))
+  const [balloons, setBalloons] = useState(balloonArr)
+  const onPop = (index) => {
+    setBalloons((balloons) => {
+      const b = [...balloons]
+      console.log(b)
+      b[index].popped = true
+      return b
+    })
+  }
+  return (
+    <div>
+      {balloons.map((b, i) => {
+        if (!b.popped) {
+          return (
+            <div className="balloon-container" key={b.id}>
+              <div
+                className="balloon"
+                style={{ backgroundColor: b.color }}
+                onClick={() => onPop(i)}
+              ></div>
+            </div>
+          )
+        } else {
+          return (
+            <div className="balloon-container" key={i}>
+              <div className="popped-balloon"></div>
+            </div>
+          )
+        }
+      })}
+    </div>
+  )
 }
 
 export default Balloon
