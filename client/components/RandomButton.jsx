f33ff96b332861f307713591c1aa68f122fce020
Shweta-Chaubey-Hihi-22 <112045002+shwetachaubey2022@users.noreply.github.com>
2022-11-03 16:03:07 +1300

    Created RandomButton component

---
+++ client/components/RandomButton.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+
+function RandomButton(){
+  const originalColour=
+  const [newColour,setNewColour ]= useState('red')
+}
+
+
+// setState of balloon colour
+// newState random colour
+// function to choose random colour
+// event handler to handle button click
+// button 
